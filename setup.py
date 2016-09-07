758a5b8f93e5876279a996770f5b4a71f952c8c5
German Lashevich <german.lashevich@tatar.ru>
2016-09-07 20:35:59 +0300

    Prepared for PyPI.

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@
 from setuptools import setup
 
 setup(
-    name         = 'powerline-taskwarrior',
-    description  = 'A Powerline segment for showing information from Taskwarrior task manager',
-    version      = '0.1',
-    keywords     = 'powerline taskwarrior context prompt',
-    license      = 'MIT',
-    author       = 'German Lashevich',
-    author_email = 'german.lashevich@gmail.com',
-    url          = 'https://github.com/zebradil/powerline-taskwarrior',
-    packages     = ['powerline_taskwarrior'],
-    classifiers  = [
+    name='powerline-taskwarrior',
+    description='A Powerline segment for showing information from Taskwarrior task manager',
+    version='0.1',
+    keywords='powerline taskwarrior context prompt',
+    license='MIT',
+    author='German Lashevich',
+    author_email='german.lashevich@gmail.com',
+    url='https://github.com/zebradil/powerline-taskwarrior',
+    download_url='https://github.com/zebradil/powerline-taskwarrior/tarball/0.1',
+    packages=['powerline_taskwarrior'],
+    classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
