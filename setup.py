bfcba95de3363d01652a1b3339cb399869b45d32
German Lashevich <german.lashevich@gmail.com>
2017-05-12 15:17:53 +0300

    Bump v0.5

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 
 from setuptools import setup
 
+version = '0.5'
+
 setup(
     name='powerline-taskwarrior',
     description='Powerline segments for showing information from the Taskwarrior task manager',
-    version='0.4.1',
+    version=version,
     keywords='powerline taskwarrior context prompt',
     license='MIT',
     author='German Lashevich',
     author_email='german.lashevich@gmail.com',
     url='https://github.com/zebradil/powerline-taskwarrior',
-    download_url='https://github.com/zebradil/powerline-taskwarrior/tarball/0.4.1',
+    download_url='https://github.com/zebradil/powerline-taskwarrior/tarball/{version}'.format(version=version),
     packages=['powerline_taskwarrior'],
     classifiers=[
         'Environment :: Console',
