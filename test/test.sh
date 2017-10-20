332c12d0afc1b2eec685ca20c8036e0e793ed587
German Lashevich <german.lashevich@gmail.com>
2017-10-20 18:28:17 +0200

    Print error in test

--- test/test.sh
+++ test/test.sh
@@ -7,4 +7,10 @@ pip${PYTHON_VERSION} install .
 TASK_TEXT=$(date | md5sum | head -c 16)
 task add $TASK_TEXT
 export XDG_CONFIG_DIRS=$PTW_DIR/test/.config
-[[ "$(powerline shell right | fgrep -c $TASK_TEXT)" -eq 1 ]] || exit 1
+LOG_FILE=/tmp/powerline-error.log
+if [[ "$(powerline shell right | tee $LOG_FILE | fgrep -c $TASK_TEXT)" -eq 1 ]]; then
+  exit 0
+else
+  cat $LOG_FILE
+  exit 1
+fi
