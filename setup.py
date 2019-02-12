5e397f1414bce94e83f718e531e5a3a4aa2d3d37
German Lashevich <german.lashevich@gmail.com>
2019-02-12 21:33:08 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,26 +2,24 @@
 
 from setuptools import setup
 
-version = '0.6.2'
+version = "0.7.0"
 
 setup(
-    name='powerline-taskwarrior',
-    description='Powerline segments for showing information from the Taskwarrior task manager',
+    name="powerline-taskwarrior",
+    description="Powerline segments for showing information from the Taskwarrior task manager",
     version=version,
-    keywords='powerline taskwarrior context prompt',
-    license='MIT',
-    author='German Lashevich',
-    author_email='german.lashevich@gmail.com',
-    url='https://github.com/zebradil/powerline-taskwarrior',
-    download_url='https://github.com/zebradil/powerline-taskwarrior/tarball/{version}'.format(version=version),
-    packages=['powerline_taskwarrior'],
-    install_requires=[
-        'powerline-status',
-    ],
+    keywords="powerline taskwarrior context prompt",
+    license="MIT",
+    author="German Lashevich",
+    author_email="german.lashevich@gmail.com",
+    url="https://github.com/zebradil/powerline-taskwarrior",
+    download_url="https://github.com/zebradil/powerline-taskwarrior/tarball/{version}".format(version=version),
+    packages=["powerline_taskwarrior"],
+    install_requires=["powerline-status"],
     classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Terminals'
-    ]
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Terminals",
+    ],
 )
