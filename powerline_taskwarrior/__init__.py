fbb1fdfef3deabcae53bb6c8b1d9679436ea7bcb
German Lashevich <german.lashevich@gmail.com>
2021-10-09 23:04:46 +0200

    Apply black and isort

--- powerline_taskwarrior/__init__.py
+++ powerline_taskwarrior/__init__.py
@@ -1,5 +1,2 @@
-from .segments import active_task
-from .segments import context
-from .segments import next_task
-from .segments import pending_tasks_count
-from .segments import taskwarrior
+from .segments import (active_task, context, next_task, pending_tasks_count,
+                       taskwarrior)
--- powerline_taskwarrior/segments.py
+++ powerline_taskwarrior/segments.py
@@ -9,29 +9,29 @@ from powerline.theme import requires_segment_info
 @requires_segment_info
 class TaskwarriorBaseSegment(Segment):
     pl = None
-    task_alias = 'task'
+    task_alias = "task"
 
     def execute(self, command):
-        self.pl.debug('Executing command: %s' % ' '.join(command))
+        self.pl.debug("Executing command: %s" % " ".join(command))
 
         proc = Popen(command, stdout=PIPE, stderr=PIPE)
-        out, err = [item.decode('utf-8') for item in proc.communicate()]
+        out, err = [item.decode("utf-8") for item in proc.communicate()]
 
         if out:
-            self.pl.debug('Command output: %s' % out.strip(string.whitespace))
+            self.pl.debug("Command output: %s" % out.strip(string.whitespace))
         if err:
-            self.pl.debug('Command errors: %s' % err.strip(string.whitespace))
+            self.pl.debug("Command errors: %s" % err.strip(string.whitespace))
 
         return out.splitlines(), err.splitlines()
 
     def build_segments(self):
-        self.pl.debug('Nothing to do')
+        self.pl.debug("Nothing to do")
         return []
 
-    def __call__(self, pl, segment_info, task_alias='task'):
+    def __call__(self, pl, segment_info, task_alias="task"):
         self.pl = pl
         self.task_alias = task_alias
-        pl.debug('Running Taskwarrior: ' + task_alias)
+        pl.debug("Running Taskwarrior: " + task_alias)
 
         if not task_alias:
             return
@@ -41,17 +41,21 @@ class TaskwarriorBaseSegment(Segment):
 
 class ContextSegment(TaskwarriorBaseSegment):
     def build_segments(self):
-        self.pl.debug('Build Context segment')
+        self.pl.debug("Build Context segment")
 
-        context_name, err = self.execute([self.task_alias, 'rc.hooks:off', '_get', 'rc.context'])
+        context_name, err = self.execute(
+            [self.task_alias, "rc.hooks:off", "_get", "rc.context"]
+        )
 
         if not err and context_name:
             context_name = context_name.pop(0)
             if context_name:
-                return [{
-                    'contents': context_name,
-                    'highlight_groups': ['taskwarrior:context'],
-                }]
+                return [
+                    {
+                        "contents": context_name,
+                        "highlight_groups": ["taskwarrior:context"],
+                    }
+                ]
 
         return []
 
@@ -60,11 +64,13 @@ class ActiveTaskSegment(TaskwarriorBaseSegment):
     description = None
     task_id = None
 
-    def __call__(self, pl, segment_info, task_alias='task', description_length=40, state="active"):
+    def __call__(
+        self, pl, segment_info, task_alias="task", description_length=40, state="active"
+    ):
         self.pl = pl
         self.task_alias = task_alias
         self.state = state
-        pl.debug('Running Taskwarrior: ' + task_alias)
+        pl.debug("Running Taskwarrior: " + task_alias)
 
         if not task_alias:
             return
@@ -72,34 +78,39 @@ class ActiveTaskSegment(TaskwarriorBaseSegment):
         return self.build_segments(description_length)
 
     def build_segments(self, description_length=0):
-        self.pl.debug('Build ActiveTask segment')
+        self.pl.debug("Build ActiveTask segment")
 
         task = self.get_task()
 
         if task:
             self.task_id, self.description = task
 
-            return [{
-                'name': 'active_task_id',
-                'contents': '#%s' % self.task_id,
-                'highlight_groups': [
-                    'taskwarrior:{state}_id'.format(state=self.state)
-                 ],
-            }, {
-                'name': 'active_task_description',
-                'contents': self.cut_description(self.description, description_length),
-                'highlight_groups': [
-                    'taskwarrior:{state}_desc'.format(state=self.state)
-                ],
-            }]
+            return [
+                {
+                    "name": "active_task_id",
+                    "contents": "#%s" % self.task_id,
+                    "highlight_groups": [
+                        "taskwarrior:{state}_id".format(state=self.state)
+                    ],
+                },
+                {
+                    "name": "active_task_description",
+                    "contents": self.cut_description(
+                        self.description, description_length
+                    ),
+                    "highlight_groups": [
+                        "taskwarrior:{state}_desc".format(state=self.state)
+                    ],
+                },
+            ]
         else:
             return []
 
     def truncate(self, pl, amount, segment, description_length=0, **kwargs):
-        if segment['name'] == 'active_task_id':
-            return segment['contents']
+        if segment["name"] == "active_task_id":
+            return segment["contents"]
 
-        length = segment['_len'] - amount
+        length = segment["_len"] - amount
         if description_length and description_length < length:
             length = description_length
         return self.cut_description(self.description, length)
@@ -109,10 +120,10 @@ class ActiveTaskSegment(TaskwarriorBaseSegment):
         if length and len(description) > length:
             parts = []
             for part in description.split():
-                if len(' '.join(parts + [part])) < length - 1:
+                if len(" ".join(parts + [part])) < length - 1:
                     parts.append(part)
                 else:
-                    return ' '.join(parts) + '…'
+                    return " ".join(parts) + "…"
         else:
             return description
 
@@ -120,27 +131,36 @@ class ActiveTaskSegment(TaskwarriorBaseSegment):
         id_and_description, err = self.execute(self.get_command_parts())
 
         if not err and id_and_description:
-            return id_and_description.pop(0).split(' ', 1)
+            return id_and_description.pop(0).split(" ", 1)
 
     def get_command_parts(self):
         return [
             self.task_alias,
-            'rc.verbose:',
-            'rc.hooks:off',
-            'rc.report.next.columns:id,description',
-            'rc.report.next.labels:1,2',
-            'limit:1',
-            '+ACTIVE',
-            'next'
+            "rc.verbose:",
+            "rc.hooks:off",
+            "rc.report.next.columns:id,description",
+            "rc.report.next.labels:1,2",
+            "limit:1",
+            "+ACTIVE",
+            "next",
         ]
 
 
 class NextTaskSegment(ActiveTaskSegment):
-    def __call__(self, pl, segment_info, task_alias='task', description_length=40, ignore_active=False):
+    def __call__(
+        self,
+        pl,
+        segment_info,
+        task_alias="task",
+        description_length=40,
+        ignore_active=False,
+    ):
         self.pl = pl
         self.task_alias = task_alias
         if ignore_active or not self.exists_active_task():
-            return super(NextTaskSegment, self).__call__(pl, segment_info, task_alias, description_length, state="next")
+            return super(NextTaskSegment, self).__call__(
+                pl, segment_info, task_alias, description_length, state="next"
+            )
         else:
             return []
 
@@ -151,85 +171,90 @@ class NextTaskSegment(ActiveTaskSegment):
     def get_command_parts(self):
         return [
             self.task_alias,
-            'rc.verbose:',
-            'rc.hooks:off',
-            'rc.report.next.columns:id,description',
-            'rc.report.next.labels:1,2',
-            'limit:1',
-            'next'
+            "rc.verbose:",
+            "rc.hooks:off",
+            "rc.report.next.columns:id,description",
+            "rc.report.next.labels:1,2",
+            "limit:1",
+            "next",
         ]
 
 
 class PendingTasksCountSegment(TaskwarriorBaseSegment):
     def build_segments(self):
-        self.pl.debug('Build PendingTasksCount segment')
+        self.pl.debug("Build PendingTasksCount segment")
 
-        pending_tasks_count, err = self.execute([
-            self.task_alias,
-            'rc.verbose:',
-            'rc.hooks:off',
-            'status:pending',
-            'count'
-        ])
+        pending_tasks_count, err = self.execute(
+            [self.task_alias, "rc.verbose:", "rc.hooks:off", "status:pending", "count"]
+        )
 
         if not err and pending_tasks_count:
             pending_tasks_count = pending_tasks_count.pop(0)
             if pending_tasks_count:
-                return [{
-                    'contents': '[%s]' % pending_tasks_count,
-                    'highlight_groups': ['taskwarrior:context'],
-                }]
+                return [
+                    {
+                        "contents": "[%s]" % pending_tasks_count,
+                        "highlight_groups": ["taskwarrior:context"],
+                    }
+                ]
 
         return []
 
 
 class TaskwarriorSegment(TaskwarriorBaseSegment):
     def build_segments(self):
-        self.pl.debug('Build ActiveTask + Context segment')
-        return ActiveTaskSegment()(self.pl, self.task_alias) + ContextSegment()(self.pl, self.task_alias)
+        self.pl.debug("Build ActiveTask + Context segment")
+        return ActiveTaskSegment()(self.pl, self.task_alias) + ContextSegment()(
+            self.pl, self.task_alias
+        )
 
 
 taskwarrior = with_docstring(
     TaskwarriorSegment(),
-    '''Return information from Taskwarrior task manager.
+    """Return information from Taskwarrior task manager.
 
     It will show current context and active task (first by urgency order).
 
     Highlight groups used: ``taskwarrior:active_id``, ``taskwarrior:active_desc``, ``taskwarrior:context``
-    ''')
+    """,
+)
 
 context = with_docstring(
     ContextSegment(),
-    '''Return information from Taskwarrior task manager.
+    """Return information from Taskwarrior task manager.
 
     It will show current context.
 
     Highlight groups used: ``information:regular``
-    ''')
+    """,
+)
 
 active_task = with_docstring(
     ActiveTaskSegment(),
-    '''Return information from Taskwarrior task manager.
+    """Return information from Taskwarrior task manager.
 
     It will show active task (first by urgency order).
 
     Highlight groups used: ``taskwarrior:active_id``, ``taskwarrior:active_desc``
-    ''')
+    """,
+)
 
 next_task = with_docstring(
     NextTaskSegment(),
-    '''Return information from Taskwarrior task manager.
+    """Return information from Taskwarrior task manager.
 
     It will show next task (first by urgency order).
 
     Highlight groups used: ``taskwarrior:next_id``, ``taskwarrior:next_desc``
-    ''')
+    """,
+)
 
 pending_tasks_count = with_docstring(
     PendingTasksCountSegment(),
-    '''Return information from Taskwarrior task manager.
+    """Return information from Taskwarrior task manager.
 
     It will show count of pending tasks.
 
     Highlight groups used: ``taskwarrior:next_id``, ``taskwarrior:next_desc``
-    ''')
+    """,
+)
