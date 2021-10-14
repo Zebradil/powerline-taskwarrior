cb17826733fc70d3b1f17baa440d792e0f4cebbd
German Lashevich <german.lashevich@gmail.com>
2021-10-14 11:14:55 +0200

    Enhance test script output

--- test/test.sh
+++ test/test.sh
@@ -12,9 +12,13 @@ export XDG_CONFIG_DIRS="$(dirname -- "${BASH_SOURCE[0]}")/.config"
 
 LOG_FILE=/tmp/powerline-error.log
 
-if [[ "$(powerline shell right | tee $LOG_FILE | fgrep -c $TASK_TEXT)" -eq 1 ]]; then
-    exit 0
-else
+if [[ "$(powerline shell right | tee $LOG_FILE | fgrep -c $TASK_TEXT)" -ne 1 ]]; then
+    echo -e "\033[1;31m======== TEST FAILED ========\033[0m"
+    echo The output expected to contain "$TASK_TEXT"
+    echo The following was printed instead:
     cat $LOG_FILE
+    echo
     exit 1
 fi
+
+echo -e "\033[1;32m======== TEST PASSED ========\033[0m"
